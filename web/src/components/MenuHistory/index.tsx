b748bbee2f75d9236f0fafb58ec7590075cbd6e3
katsuyukishimbo <katsuyuki0611@gmail.com>
2023-05-13 10:49:58 +0900

    feat: 履歴の追加

---
+++ web/src/components/MenuHistory/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const MenuHistory: React.FC = () => {
+  return (
+    <div className="container mx-auto py-4">
+      <h1 className="text-2xl font-bold mb-4">献立履歴</h1>
+      {/* 献立履歴の表示ロジックをここに実装 */}
+    </div>
+  )
+}
+
+export default MenuHistory
