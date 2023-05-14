dc864ef8ddd11c01a5e4a7bdbd3cd52ada11eba8
katsuyukishimbo <katsuyuki0611@gmail.com>
2023-05-14 13:09:22 +0900

    feat: カテゴリーのコンポーネント作成

---
+++ web/src/components/CategoryList/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const CategoryList: React.FC = () => {
+  return (
+    <div className="container mx-auto py-4">
+      <h1 className="text-2xl font-bold mb-4">カテゴリ一覧</h1>
+      {/* カテゴリ一覧の表示ロジックをここに実装 */}
+    </div>
+  )
+}
+
+export default CategoryList
