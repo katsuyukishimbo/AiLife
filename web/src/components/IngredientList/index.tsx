0dfd9fae17bb847473e312f2f392b95b405fb722
katsuyukishimbo <katsuyuki0611@gmail.com>
2023-05-14 13:09:50 +0900

    feat: 材料のコンポーネント作成

---
+++ web/src/components/IngredientList/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const IngredientList: React.FC = () => {
+  return (
+    <div className="container mx-auto py-4">
+      <h1 className="text-2xl font-bold mb-4">食材リスト</h1>
+      {/* 食材リストの表示ロジックをここに実装 */}
+    </div>
+  )
+}
+
+export default IngredientList
