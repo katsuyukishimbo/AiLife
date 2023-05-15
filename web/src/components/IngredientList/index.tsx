a042c32b50f1619893765d38aaea5bc60c8dd2bb
katsuyukishimbo <katsuyuki0611@gmail.com>
2023-05-15 20:21:49 +0900

    fix; 材料リストに仮データ用意

--- web/src/components/IngredientList/index.tsx
+++ web/src/components/IngredientList/index.tsx
@@ -1,10 +1,38 @@
 import React from "react"
+import { useNavigate } from "react-router-dom"
 
-const IngredientList: React.FC = () => {
+interface IngredientListProps {
+  // 必要に応じてプロパティを追加
+}
+
+const IngredientList: React.FC<IngredientListProps> = () => {
+  const navigate = useNavigate()
+
+  // APIからデータを取得するための仮のデータ
+  const ingredients = [
+    { id: 1, name: "Ingredient 1" },
+    { id: 2, name: "Ingredient 2" },
+    { id: 3, name: "Ingredient 3" },
+  ]
+
+  const handleBackClick = () => {
+    navigate("/menu-suggestion")
+  }
+
   return (
     <div className="container mx-auto py-4">
       <h1 className="text-2xl font-bold mb-4">食材リスト</h1>
-      {/* 食材リストの表示ロジックをここに実装 */}
+      <ul>
+        {ingredients.map((ingredient) => (
+          <li key={ingredient.id}>{ingredient.name}</li>
+        ))}
+      </ul>
+      <button
+        onClick={handleBackClick}
+        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Back to Menu Suggestion
+      </button>
     </div>
   )
 }
